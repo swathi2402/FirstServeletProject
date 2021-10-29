f63f4caf3e3c8ad0f6b938e4695ec09d1452b8fa
Swathi <swathi.h@perfios.com>
2021-10-29 16:20:13 +0530

    ADDED: Createing servelet class to display message in web page

---
+++ src/main/java/firstsevelet/FirstSevelet.java
@@ -0,0 +1,22 @@
+package firstsevelet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FirstServlet")
+public class FirstSevelet extends HttpServlet {
+
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Welcome to my First Servlet</h3>");
+        out.close();
+    }
+}
