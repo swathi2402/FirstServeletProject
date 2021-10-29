b6feaea147507e8101f91333727b378dd6f98563
Swathi <swathi.h@perfios.com>
2021-10-29 16:48:41 +0530

    MODIFIED: Adding RegEx to validate entered name

--- src/main/java/firstsevelet/LoginServlet.java
+++ src/main/java/firstsevelet/LoginServlet.java
@@ -2,10 +2,10 @@ package firstsevelet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,31 +13,29 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
         description = "Login servlet",
-        urlPatterns = {"/LoginServlet"},
-        initParams = {
-                @WebInitParam(name="user", value="swathi"),
-                @WebInitParam(name="password", value="swathi123"),
-        }
+        urlPatterns = {"/LoginServlet"}       
 )
 
 public class LoginServlet extends HttpServlet {
 
 	@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String user = request.getParameter("user");
+		String user = request.getParameter("user");
         String pwd = request.getParameter("pwd");
-        String userID = getServletConfig().getInitParameter("user");
-        String password = getServletConfig().getInitParameter("password");
-        if(userID.equals(user) && password.equals(pwd)){
+
+        if (isNameValid(user) ) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
 
-        }else{
+        } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either username or password is wrong</font>");
+            out.println("<font color=red>Invalid Username Or Password</font>");
             rd.include(request, response);
         }
-
     }
+	
+	 private boolean isNameValid(String name) {
+	        return Pattern.matches("^[A-Z][a-zA-Z]{2,}$", name);
+	    }     
 }
